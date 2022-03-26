dd6ef4ab15c5c40d64214deab90c476446629ce4
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-03-26 06:44:56 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <stack>
+#include <map>
+#include <string>
+using namespace std;
+
+class PostfixNotation {
+public:
+    static string toPostfixNotation(const string& expr);
+    static double calculatePostfixExpression(const string& expr);
+};
+
+map<char, int> operations = { {'+', 0}, {'-', 0},  {'*', 1}, {'/', 1} };
+string PostfixNotation::toPostfixNotation(const string& expr) {
+    stack<char> stack;
+    string result;
+    for (const auto& i : expr) {
+        if (isdigit(i)) {
+            result += i;
+            continue;
+        }
+        if (operations.find(i) != operations.end()) {
+            if (stack.empty() || stack.top() == '(') {
+                stack.push(i);
+                continue;
+            }
+            if ((operations.find(stack.top()) != operations.end()) && operations[i] > operations[stack.top()]) {
+                stack.push(i);
+                continue;
+            }
+            if ((operations.find(stack.top()) != operations.end()) && operations[i] <= operations[stack.top()]) {
+                while ((operations.find(stack.top()) != operations.end() && operations[i] <= operations[stack.top()]) || stack.top() != '(') {
+                    result += stack.top();
+                    stack.pop();
+                    if (stack.empty())
+                        break;
+                }
+                stack.push(i);
+                continue;
+            }
+        }
+        if (i == '(') {
+            stack.push(i);
+            continue;
+        }
+        if (i == ')') {
+            while (stack.top() != '(') {
+                result += stack.top();
+                stack.pop();
+            }
+            stack.pop();
+        }
+    }
+    while (!stack.empty()) {
+        result += stack.top();
+        stack.pop();
+    }
+    return result;
+}
+
+double PostfixNotation::calculatePostfixExpression(const string& expr) {
+    stack<string>stack;
+    for (const auto& i : expr) {
+        if (isdigit(i)) {
+            stack.push(string(1, i));
+            continue;
+        }
+        if (operations.find(i) != operations.end()) {
+            auto second = stod(stack.top());
+            stack.pop();
+            auto first = stod(stack.top());
+            stack.pop();
+            if (i == '*')
+                stack.push(to_string(first * second));
+            if (i == '/')
+                stack.push(to_string(first / second));
+            if (i == '-')
+                stack.push(to_string(first - second));
+            if (i == '+')
+                stack.push(to_string(first + second));
+        }
+    }
+    return stod(stack.top());
+}
+
+
+int main()
+{
+    string postfix = PostfixNotation::toPostfixNotation("((2+1)*3+5)*(1+1)");
+    cout << postfix << endl;
+    double result = PostfixNotation::calculatePostfixExpression(postfix);
+    cout << result;
+}
---
+++ 4.cpp
@@ -0,0 +1,145 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+
+struct Node {
+    int data;
+    Node* next = nullptr;
+};
+
+class EnclosedList {
+private:
+    Node* head = nullptr;
+    Node* currentStone = nullptr;
+public:
+    EnclosedList(ifstream& readFrom);
+    ~EnclosedList();
+
+    void printList();
+    vector<int> takeThreeStones();
+    Node* findMaxStone();
+    void putAfterFound(vector<int>&, Node* maxStone);
+};
+
+
+
+EnclosedList::EnclosedList(ifstream& readFrom) {
+    Node* firstNode = new Node;
+    this->head = firstNode;
+    this->currentStone = firstNode;
+
+    int data;
+    readFrom >> data;
+    firstNode->data = data;
+
+    Node* tmp = firstNode;
+    while (!readFrom.eof()) {
+        Node* nextNode = new Node;
+        readFrom >> data;
+        nextNode->data = data;
+        tmp->next = nextNode;
+        tmp = nextNode;
+    }
+    tmp->next = this->head;
+}
+
+void EnclosedList::printList() {
+    Node* tmp = this->head;
+    cout << tmp->data << " ";
+    tmp = tmp->next;
+    while (tmp != this->head) {
+        cout << tmp->data << " ";
+        tmp = tmp->next;
+    }
+}
+
+EnclosedList::~EnclosedList() {
+    Node* tmp = this->head;
+    tmp = tmp->next;
+    while (tmp != this->head) {
+        Node* tmp2 = tmp;
+        tmp = tmp->next;
+        delete tmp2;
+    }
+    delete tmp;
+}
+
+vector<int> EnclosedList::takeThreeStones() {
+    Node* tmp = this->currentStone;
+    vector<int> stones;
+    tmp = tmp->next;
+    for (int i = 0; i < 3; i++) {
+        stones.push_back(tmp->data);
+        Node* tmp2 = tmp;
+        tmp = tmp->next;
+        if (tmp2 == this->head)
+            this->head = tmp;
+        delete tmp2;
+    }
+    this->currentStone->next = tmp;
+    return stones;
+}
+
+Node* EnclosedList::findMaxStone() {
+    Node* tmp = this->currentStone->next;
+    int currentStoneNumber = this->currentStone->data;
+    int maxNumber = -1;
+    Node* newMaxStone = nullptr;
+    while (tmp != this->currentStone) {
+        if (tmp->data > maxNumber && tmp->data < currentStoneNumber) {
+            maxNumber = tmp->data;
+            newMaxStone = tmp;
+        }
+        tmp = tmp->next;
+    }
+    if (maxNumber != -1)
+        return newMaxStone;
+    tmp = this->currentStone->next;
+    while (tmp != this->currentStone) {
+        if (tmp->data > maxNumber) {
+            maxNumber = tmp->data;
+            newMaxStone = tmp;
+        }
+        tmp = tmp->next;
+    }
+    return newMaxStone;
+}
+
+void EnclosedList::putAfterFound(vector<int>& stones, Node* maxStone) {
+    Node* tmp = maxStone;
+    Node* lastTie = maxStone->next;
+
+    for (int i = 0; i < 3; i++) {
+        Node* newNode = new Node;
+        newNode->data = stones[i];
+        if (i == 0)
+            maxStone->next = newNode;
+        if (i != 0)
+            tmp->next = newNode;
+        tmp = tmp->next;
+    }
+    tmp->next = lastTie;
+    this->currentStone = lastTie;
+}
+
+
+
+
+int main()
+{
+    ifstream input("permutation.txt");
+    EnclosedList list(input);
+    cout << "Enter the amount of permutations" << endl;
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        auto threeStones = list.takeThreeStones();
+        auto maxStone = list.findMaxStone();
+        list.putAfterFound(threeStones, maxStone);
+    }
+    list.printList();
+    return 0;
+}
